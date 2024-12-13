7837f952febc883c38b5d77c7875ec44259b9eda
Benjamin <benjaminmuratovic5@gmail.com>
2024-12-13 21:45:57 +0100

    Ispravak-MyAppUser

--- backend/RS1_2024_25.API/Data/Models/Auth/MyAppUser.cs
+++ backend/RS1_2024_25.API/Data/Models/Auth/MyAppUser.cs
@@ -12,37 +12,21 @@ public class MyAppUser:IMyBaseEntity
     public string Username { get; set; }
     [JsonIgnore]
     public string Password { get; set; }
-
-    // Additional properties
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    public string UserType { get; set; }
+    public UserType UserType { get; set; }
 
     [ForeignKey(nameof(City))]
-    public int CityId { get; set; }
+    public int? CityId { get; set; }
     public City? City { get; set; }
    
-    //----------------
-    public bool IsAdmin { get; set; }
-    public bool IsTutor { get; set; }
-    public bool IsStudent { get; set; }
-
-    /*
-     
-     Ako sistem nije zamišljen da podržava česte promjene rola i 
-     ako se dodavanje novih rola svodi na manje promjene u kodu, 
-    tada može biti dovoljno koristiti boolean polja kao što su IsAdmin, IsManager itd. 
+    public string? Gender { get; set; }
     
-    Ovaj pristup je jednostavan i efektivan u situacijama gdje su role stabilne i unaprijed definirane.
-
-    Međutim, glavna prednost korištenja role entiteta dolazi do izražaja kada aplikacija potencijalno raste i 
-    zahtjeva kompleksnije role i ovlaštenja. U scenarijima gdje se očekuje veći broj različitih rola ili kompleksniji 
-    sistem ovlaštenja, dodavanje nove bool varijable može postati nepraktično i otežati održavanje.
-
-    Dakle, za stabilne sisteme s manjim brojem fiksnih rola, boolean polja su sasvim razumno rješenje.
-     */
-
 }
+
+public enum UserType { Admin, Tutor, Student}
+
+public enum Gender { Male, Female}
