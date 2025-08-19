89ba58a8e5c3e36b64c4c72a6ede558b1146d2c4
Selma Alagić <alagic.selma@edu.fit.ba>
2025-08-19 20:49:10 +0000

    Merged PR 56: Added form validation for contact-us page
    Added all necessary validations for the form fields, along with success and error snackbars after submission

--- frontend/RS1_2024_25_angular_template/src/app/modules/public/contact-us/contact-us.component.ts
+++ frontend/RS1_2024_25_angular_template/src/app/modules/public/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+  UserForm: FormGroup;
 
+  constructor(private snackBar: MatSnackBar) {
+    this.UserForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.pattern(/^\p{L}+(?:[ '\-]?\p{L}+)*$/u)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required, Validators.minLength(10)])
+    });
+  }
+  submit() {
+    this.UserForm.markAllAsTouched();
+    if (this.UserForm.valid) {
+      this.snackBar.open('Slanje poruke uspješno!', '', {
+        duration: 4000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        panelClass: ['custom-snackbar-success']
+      })
+    }
+    else {
+      this.snackBar.open('Forma nije validna!', '', {
+        duration: 4000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        panelClass: ['custom-snackbar-error']
+      });
+    }
+  }
 }
